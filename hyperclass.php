6298995af2c5a81d39b16dbd1bcc49c2b6b65684
Hyper Studio <40313260+sharpaddroot@users.noreply.github.com>
2021-01-23 01:23:34 +0700

    Add files via upload

---
+++ hyperclass.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+* HYPER STUDIO API TrueWallet Gift Class
+ *
+ * @package   api-truewallet-gift-class
+ * @author    Hyper Studio <https://www.facebook.com/pagehyperstudio>
+ * @copyright Copyright (c) 2020-2021
+ * @link      https://github.com/sharpaddroot/api-truewallet-gift-class
+ * @version   1.0.0 (Beta)
+ *
+**/
+
+class Hyper{
+
+    public $apikey = ' '; //นำ API Key มาใส่ที่นี่
+
+    public $startpoint = 'https://hyperstu.tk/plugin/hypertruewalletapi.php';
+    function hyperRequest($giftlink){
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $this->startpoint.'?k='.$this->apikey.'&l='.$giftlink,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array('Content-Type: application/json',),
+        ));
+
+        $this->response = curl_exec($curl);
+
+        curl_close($curl);
+        $this->data = json_decode($this->response, true);
+        return $this->data;
+    }
+
+}
+
+?>
